6697137bb52de43749f3c7594e2811927855bbab
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-09-01 14:30:18 +0200

    update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,3 +1,10 @@
 version=v1.2
+versionCode=12
+
+# module.prop
+sed -e "s/version=.*/version=${version}/" module.prop > module.prop.tmp && mv module.prop.tmp module.prop
+sed -e "s/versionCode=.*/versionCode=${versionCode}/" module.prop > module.prop.tmp && mv module.prop.tmp module.prop
+
+# zip
 rm -f perf-limit-xiaomi-12*.zip
 zip -vr perf-limit-xiaomi-12-$version.zip . -x "*.DS_Store" -x "*.zip" -x ".git/*" -x ".gitignore" -x "zip.sh" -x "test.sh"
