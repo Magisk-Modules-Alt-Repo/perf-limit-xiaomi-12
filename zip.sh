0f7f6458b31a463d4aa40ae9e3f6e78852eafa93
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-09-02 18:47:28 +0200

    v2.3 remove docs from zip file

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
-version=v2.2
-versionCode=22
+version=v2.3
+versionCode=23
 
 # module.prop
 sed -e "s/version=.*/version=${version}/" module.prop > module.prop.tmp && mv module.prop.tmp module.prop
@@ -7,4 +7,4 @@ sed -e "s/versionCode=.*/versionCode=${versionCode}/" module.prop > module.prop.
 
 # zip
 rm -f perf-limit-xiaomi-12*.zip
-zip -vr perf-limit-xiaomi-12-$version.zip . -x "*.DS_Store" -x "*.zip" -x ".git/*" -x ".gitignore" -x ".github" -x "README.md" -x "LICENSE" -x "docs" -x "zip.sh" -x "test.sh"
+zip -vr perf-limit-xiaomi-12-$version.zip . -x "*.DS_Store" -x "*.zip" -x ".git/*" -x ".gitignore" -x ".github/*" -x "README.md" -x "LICENSE" -x "docs/*" -x "zip.sh" -x "test.sh"
